c8bd0d42516ce5cef0406b99fe471ac26efa878b
3Emme <emmettbuentiempo@gmail.com>
2020-08-19 14:19:13 -0700

    add initial structure of pig-latin function, including vowel and consonant arrays in addition to first attempt at translation function.
    Co-authored-by: Drew Parker <drewjparker91@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $("form#pig-latin-form").submit(function(event){
+    event.preventDefault();
+    const vowelArray = ["a", "i", "e", "o", "u"];
+    const consonantArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+    const string = "No way.";
+    const wordsArray = string.split(" ");
+    //wordsArray = ["No", "way."]
+    //translatedWordsArray = []
+
+    return lettersArray.map(function(element) {
+      const lettersArray = wordsArray.split("");
+      if (vowels.includes(userInput[0])) 
+        return userInput + "way";
+    });
+    const wordsArray = string.split("");
+    let vowelIndex = [0];
+    
+
+  });
+});
+
+
+
+
+function pigLatin(str) {}
+  str = str.toLowerCase()
+  const vowelArray = ["a", "i", "e", "o", "u"];
+  let vowelIndex = 0;
+
+  if (vowels.includes(str[0]) {
+    return str + "way"
+  } else }
+  for (let 
